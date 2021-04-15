add18be9c6fdfae0785647d36be0e0de48fcbb2c
Tylerve9 <tyler.ve@gmail.com>
2021-04-15 00:35:39 -0400

    Primer Commit del Ejercicio simple counter react realizado en su forma basica

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,25 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import SecondsCounter from "./secondsCounter";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+var seconds = 0;
+const initMoment = Date.now();
+var rendering;
+let changer = setInterval(() => {
+	//let currentMoment = Date.now();
 
+	ReactDOM.render(
+		<SecondsCounter seconds={seconds} />,
+		document.querySelector("#counter")
+	);
+	seconds += 1;
+	//console.log("initMoment: " + seconds);
+	// console.log("currentMoment: " + currentMoment);
+	// console.log("seconds: ", seconds);
+}, 1000);
 //create your first component
 export function Home() {
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+	//console.log(initMoment);
+	return <div id="counter"></div>;
 }
---
+++ src/js/component/secondsCounter.js
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function SecondsCounter(props) {
+	return (
+		<div className="text-center mt-5">
+			<div className="card shadow">
+				<div className="card-header bg-primary text-white font-weight-bolder">
+					Seconds Counter
+				</div>
+				<div className="card-body">
+					<h3 className="card-title">
+						Seconds <span> {props.seconds} </span>
+					</h3>
+					<p className="card-text"></p>
+					{/* <a href="#" className="btn btn-primary">
+						Go somewhere
+					</a> */}
+				</div>
+			</div>
+		</div>
+	);
+}
+
+// SecondsCounter.defaultProps = {
+// 	seconds: 0
+// };
+
+SecondsCounter.propTypes = {
+	seconds: PropTypes.number
+};
+
+export default SecondsCounter;
